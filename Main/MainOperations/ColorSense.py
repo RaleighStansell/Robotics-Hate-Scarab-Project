49fc3661550e141aa714fe5dd60f13a8edb991de
Raleigh Stansell <Raleighstansell@gmail.com>
2026-09-13 17:11:10 -0500

    Properly Tested Color Sensor

--- Main/MainOperations/ColorSense.py
+++ Main/MainOperations/ColorSense.py
@@ -1,49 +1,74 @@
 from machine import Pin
-from utime import sleep
+from utime import sleep_us, sleep
 import time
 
-class ColorSensor:
+class TCS3200:
     def __init__(self, s0, s1, s2, s3, out_pin):
-        # Control pins
-        self.s0 = Pin(s0, Pin.OUT) # Magenta Color
-        self.s1 = Pin(s1, Pin.OUT) # Blue Color
-        self.s2 = Pin(s2, Pin.OUT) # Green Color
-        self.s3 = Pin(s3, Pin.OUT) # Red Color
-        # Output pin from sensor
+        self.s0 = Pin(s0, Pin.OUT)
+        self.s1 = Pin(s1, Pin.OUT)
+        self.s2 = Pin(s2, Pin.OUT)
+        self.s3 = Pin(s3, Pin.OUT)
         self.out_pin = Pin(out_pin, Pin.IN)
 
-        # Set scaling to 20% (recommended for stability)
+        # 20% scaling for stability
         self.s0.value(1)
         self.s1.value(0)
 
-    def read_color(self, color):
-        # Set the filter for the specified color
-        if(color == 'Yellow'):
-            self.s1.value(1)
-            self.s2.value(1)
-            self.s3.value(0)
-        elif(color == 'Cyan'):
-            self.s1.value(0)
-            self.s2.value(1)
-            self.s3.value(1)
-        elif(color == 'Magenta'):
-            self.s1.value(1)
-            self.s2.value(0)
-            self.s3.value(1)
-        else:
-            self.s1.value(0)
+    def _set_filter(self, color):
+        # Correct TCS3200 truth table
+        if color == "RED":
             self.s2.value(0)
             self.s3.value(0)
+        elif color == "BLUE":
+            self.s2.value(0)
+            self.s3.value(1)
+        elif color == "CLEAR":
+            self.s2.value(1)
+            self.s3.value(0)
+        elif color == "GREEN":
+            self.s2.value(1)
+            self.s3.value(1)
 
-        # Wait for the sensor to stabilize
-        sleep(0.1)
-        # Measure the frequency of the output signal
-        start_time = time.ticks_us()
+    def _read_frequency(self):
+        start = time.ticks_us()
         count = 0
-        while time.ticks_diff(time.ticks_us(), start_time) < 100000:  # Measure for 100ms
+        while time.ticks_diff(time.ticks_us(), start) < 100000:  # 100 ms
             if self.out_pin.value() == 1:
                 count += 1
                 while self.out_pin.value() == 1:
-                    pass  # Wait for the signal to go low
+                    pass
         return count
 
+    def read_rgb(self):
+        rgb = {}
+
+        for color in ["RED", "GREEN", "BLUE"]:
+            self._set_filter(color)
+            sleep(0.02)
+            rgb[color] = self._read_frequency()
+
+        return rgb
+
+    def detect_color(self, rgb):
+        red = rgb["RED"]
+        green = rgb["GREEN"]
+        blue = rgb["BLUE"]
+        if(red>100 and blue>100 and green>100):
+            if(red > blue*1.5 and green > blue*1.5):
+                return "Yellow"
+            elif(blue*1.5 > red and blue*1.5 > green):
+                return "Cyan"
+            elif(red > green and blue*1.5 > green):
+                return "Magenta"
+        else:
+            return "Unknown"
+
+
+while True:
+
+    sensor = TCS3200(s0=2, s1=3, s2=4, s3=5, out_pin=6)
+
+    rgb = sensor.read_rgb()
+    color = sensor.detect_color(rgb)
+    print("RGB:", rgb, "Detected:", color)
+    sleep(0.2)
